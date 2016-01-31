6229d7d7917c14988a33e2fc71414ab124f0569f
Neal Johnson <nrjohnso@mtu.edu>
2016-01-31 10:17:43 -0500

    Cleaned up a lot of the BoardManager. Got rid of a lot of hard-coded things. Also, finally got that bug where wall sections would alternate being flipped. Really hacky fix btw, but it will do for now.

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -13,48 +13,55 @@ public class BoardManager : MonoBehaviour {
 
 	public Wall wallPrefab;  	// We pas in a prefab so we can create objects on the fly
 								// This will eventually need to be done for any other object
-
-	// So right now all the numbers for wall spawning is hard-coded in these
-	// values. Eventually we will need to dynamically know sizes of the sprites. 
-	// TODO make dynamic
-	public static int numberWalls = 24;
-	public float upperY = 5.5f;
-	public float lowerY = -5.5f;
-	public int spriteOffset = 2;
 	
-	private Transform boardHolder;		// Not exactly sure why this is needed, I think just as a common location origin...
-	private Queue <Wall> upperWalls = new Queue<Wall>(numberWalls);		// All upperwalls stored in this queue
-	private Queue <Wall> lowerWalls = new Queue<Wall>(numberWalls);		// Same for lower walls
-	private float lastUpperX, lastLowerX;		// We need to know the position of the last wall so we can spawn a new one next to it
+	//private Transform boardHolder;		// Not exactly sure why this is needed, I think just as a common location origin...
+	public Queue <Wall> upperWalls = new Queue<Wall>();		// All upperwalls stored in this queue
+	public Queue <Wall> lowerWalls = new Queue<Wall>();		// Same for lower walls
+	private float lastX;										// keep track of world position to spawn next wall
+	private float lowerLimit, upperLimit;						// min and max height for spawning walls
+	private float screenWidth;
+	private Vector3 spriteSize;
 
 	/**
 	 * Initialization for wall objects. Buff/debuffs should 
 	 * probably not be spawned in the initial spawned space of any level.
-	 * 
-	 * NOTE: right now this sets up a scene smaller than the camera viewport, 
-	 * its intentional so that I can see objects as they spawn.
 	 **/
-	void boardSetup () {
-		boardHolder = new GameObject("Board").transform;
+	private void boardSetup () {
+		Vector3 topLeft, bottomLeft, topRight;
+		spriteSize = getSpriteSize();
 
-		// Fill the floor and ceiling with walls. This is ugly and hardcoded for now. 
-		for(int x = 0; x < numberWalls; x = x + spriteOffset) {
-			// eventually this instantiation logic should reside in the wall object
+		//get camera bounds, so we can start spawning walls at the right place.
+		topLeft = Camera.main.ViewportToWorldPoint(new Vector3 (0, 1, 0));
+		bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3 (0, 0, 0));
+		topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+		screenWidth = topRight.x - topLeft.x;
+		lowerLimit = bottomLeft.y + (spriteSize.y / 2);
+		upperLimit = topLeft.y - (spriteSize.y / 2);
+
+		// Fill the floor and ceiling with walls across the length of the screen
+		for(lastX = topLeft.x; lastX < screenWidth; lastX += spriteSize.x) {
 			Wall upperClone;
-			upperClone = Instantiate(wallPrefab, new Vector2 (x - (numberWalls * .5f), upperY), Quaternion.identity) as Wall;
-			upperClone.transform.SetParent(boardHolder);
+			upperClone = Instantiate(wallPrefab, new Vector2 (lastX, upperLimit), Quaternion.identity) as Wall;
+			upperClone.transform.Rotate (180, 0, 0);   //Rotate the wall, eventually a circular sprite could be used...idk...
 			upperWalls.Enqueue(upperClone);
 
 			Wall lowerClone;
-			lowerClone = Instantiate (wallPrefab, new Vector2(x - (numberWalls * .5f), lowerY), Quaternion.identity) as Wall;
-			lowerClone.transform.SetParent(boardHolder);
-			lowerClone.transform.Rotate (180, 0, 0);   //Rotate the wall, eventually a circular sprite could be used...idk...
+			lowerClone = Instantiate (wallPrefab, new Vector2(lastX, lowerLimit), Quaternion.identity) as Wall;
 			lowerWalls.Enqueue(lowerClone);
 		}
 
-		// store final positions of the walls
-		lastUpperX = numberWalls * .5f;
-		lastLowerX = numberWalls * .5f;
+		// need to revert lastX or there is a gap in the walls being spawned later.
+		lastX -= spriteSize.x;
+	}
+
+	/**
+	 * Get an instantiated version of wall to measure the sprite. Don't know of a better way to do this yet
+	 */
+	private Vector3 getSpriteSize() {
+		Wall wall = Instantiate(wallPrefab, new Vector2 (-10, -10), Quaternion.identity) as Wall;
+		Vector3 spriteWidth = wall.GetComponent<SpriteRenderer>().bounds.size;
+		Destroy(wall);
+		return spriteWidth;
 	}
 
 	/**
@@ -64,16 +71,16 @@ public class BoardManager : MonoBehaviour {
 		boardSetup();
 	}
 	
-	float newYPosition(Wall lastWall){
+	private float newYPosition(Wall lastWall){
 		float nextPos = Random.Range (0, .5f) + lastWall.transform.position.y;
 		float sign = Random.Range (0, 1);
 		if (sign < .5) {
 			nextPos = nextPos * -1f;
 		}
-		if (nextPos > upperY){
-			return upperY;
-		} else if (nextPos < lowerY) {
-			return lowerY;
+		if (nextPos > upperLimit){
+			return upperLimit;
+		} else if (nextPos < lowerLimit) {
+			return lowerLimit;
 		} else {
 			return nextPos;
 		}
@@ -82,24 +89,25 @@ public class BoardManager : MonoBehaviour {
 
 	/**
 	 * Each frame, check if a wall has disappeared. If it has, spawn a new one.
+	 *
+	 * Also, I have no idea why I need to rotate all the walls every time, I just do /cry
 	 **/
 	void Update () {
-		// Check both lower and upper because eventually they may disappear at different times
 		Wall lastUpperWall = upperWalls.Peek ();
 		Wall lastLowerWall = lowerWalls.Peek ();
 		if (!lastUpperWall.GetComponent<SpriteRenderer>().isVisible) {
 			lastUpperWall = upperWalls.Dequeue ();
 			//lastUpperWall.selectNewSprite(); TODO
-			lastUpperWall.transform.position = new Vector2 (lastUpperX + spriteOffset, newYPosition(lastUpperWall));
+			lastUpperWall.transform.position = new Vector2 (lastX + spriteSize.x, newYPosition(lastUpperWall));
+			lastUpperWall.transform.Rotate (180, 0, 0);
 			upperWalls.Enqueue (lastUpperWall);
-			lastUpperX += spriteOffset;
-		}
-		if (!lastLowerWall.GetComponent<SpriteRenderer>().isVisible) {
+
 			lastLowerWall = lowerWalls.Dequeue ();
 			//lastUpperWall.selectNewSprite(); TODO
-			lastLowerWall.transform.position = new Vector2 (lastLowerX + spriteOffset, newYPosition(lastLowerWall));
+			lastLowerWall.transform.position = new Vector2 (lastX + spriteSize.x, newYPosition(lastLowerWall));
+			lastLowerWall.transform.Rotate (180, 0, 0);
 			lowerWalls.Enqueue(lastLowerWall);
-			lastLowerX += spriteOffset;
+			lastX += spriteSize.x;
 		}
 	}
 }
